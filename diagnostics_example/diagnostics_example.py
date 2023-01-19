e3d6d25873ddfe65cc52602e12fae8bc3e3d5210
nfry321 <nickfry321@gmail.com>
2023-01-19 10:23:13 +0000

    add second sensor status msg

--- diagnostics_example/diagnostics_example.py
+++ diagnostics_example/diagnostics_example.py
@@ -17,24 +17,36 @@ class DiagnosticsExample(Node):
 		)
         # Create Key-Value pairs. These contain detailed info or data related to the status
         # We can add as many as we want in an array as part of a DiagnosticStatus message
-        self.temp = KeyValue()
-        self.temp.value = str(0)
-        self.temp.key = "Motor Temp (deg)"
+        self.temp1 = KeyValue()
+        self.temp1.value = "--"
+        self.temp1.key = "Temp (deg)"
+        self.temp2 = KeyValue()
+        self.temp2.value = "--"
+        self.temp2.key = "Temp (deg)"
 
         # Create and fill a status message, one per device/module that is returning a status
-        self.status = DiagnosticStatus()
+        self.status1 = DiagnosticStatus()
         # Levels can be OK/WARN/ERROR/STALE
-        self.status.level = DiagnosticStatus.OK
-        self.status.name = "Motor Temperature"
-        self.status.message = "Initialised"
-        self.status.hardware_id = "mcp9600"
+        self.status1.level = DiagnosticStatus.OK
+        self.status1.name = "Sensor 1"
+        self.status1.message = "Initialised"
+        self.status1.hardware_id = "mcp9600 0x66"
         #add the key value pairs here
-        self.status.values.append(self.temp)
+        self.status1.values.append(self.temp1)
+
+        self.status2 = DiagnosticStatus()
+        self.status2.level = DiagnosticStatus.OK
+        self.status2.name = "Temperature Sensor"
+        self.status2.message = "Initialised"
+        self.status2.hardware_id = "mcp9600 0x67"
+        #add the key value pairs here
+        self.status2.values.append(self.temp2)
 
         # the /diagnostics topic must be published as msg type diagnosticArray
         # this is an array of all of the DiagnosticStatus msgs in the node
         self.diaArray = DiagnosticArray()
-        self.diaArray.status.append(self.status)
+        self.diaArray.status.append(self.status1)
+        self.diaArray.status.append(self.status2)
 
         # Publish initial status
         self.diagnostic_pub.publish(self.diaArray)
@@ -45,17 +57,29 @@ class DiagnosticsExample(Node):
         # If you need a heartbeat or regualar debug info you could publish using a timer
         # or just publish where you would report a status change.
         temp = random.randrange(0, 100)
-        self.temp.value = str(temp)
-        if temp < 70:
-            self.status.level = DiagnosticStatus.OK
-            self.status.message = "All good in the hood"
-        elif 71 < temp < 81:
-            self.status.level = DiagnosticStatus.WARN
-            self.status.message = "It's getting hot in here..."
-        else: 
-            self.status.level = DiagnosticStatus.ERROR
-            self.status.message = "Damn that's hot"
+        self.checkTemp(temp,1)
+        self.checkTemp(temp,2)
         self.diagnostic_pub.publish(self.diaArray)
+
+    def checkTemp(self, temp, sensor):
+        if sensor == 1:
+            status = self.status1
+            keyval = self.temp1
+        elif sensor == 2:
+            status = self.status2
+            keyval = self.temp2
+
+        keyval.value = str(temp)
+        if temp <= 50:
+            status.level = DiagnosticStatus.OK
+            status.message = "All good in the hood"
+        elif 50 < temp < 75:
+            status.level = DiagnosticStatus.WARN
+            status.message = "It's getting hot in here..."
+        else: 
+            status.level = DiagnosticStatus.ERROR
+            status.message = "Damn that's hot"
+        
 
 def main(args=None):
     rclpy.init(args=args) #required in all ros2 nodes to init ros2 communication
